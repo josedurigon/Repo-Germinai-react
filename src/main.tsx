313bfaa6d07d51bf04344841156a67245b35de42
jose durigon <jose.durigon@outlook.com>
2025-09-11 15:34:42 -0300

    arrumando referencias tokens

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
+import './styles/tokens.css';
 import "primereact/resources/themes/lara-light-blue/theme.css"; // tema
 import "primereact/resources/primereact.min.css";              // core css
 import "primeicons/primeicons.css";                            // ícones
