71a789b15f3ed6b5de549b8f704a283955a1866d
mayaracouti <mayaraferreiracout@gmail.com>
2025-11-07 03:26:02 -0300

    Implementação de todas as paginas do Sidebar e ajuste nas rotas

---
+++ src/pages/atividades/Atividades.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Atividades: React.FC = () => {
+  return <h1>Atividades</h1>;
+};
+
+export default Atividades;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+// Páginas principais
 import Login from '../pages/auth/Login';
 import Homepage from '../pages/auth/Homepage';
 import UserRegister from '../pages/user/CreateUser';
@@ -8,31 +9,47 @@ import UserList from '../pages/user/ListUsers';
 import AppLayout from '../layouts/AppLayout';
 import ProtectedRoute from './ProtectedRoute';
 import AboutUs from '../components/AboutUs/AboutUs';
-import PrevisaoPreco from '../pages/gestorInteligente/PrevisaoPreco';
 
+// Páginas específicas
+import PrevisaoPreco from '../pages/gestorInteligente/PrevisaoPreco';
+import Cadastro from '../pages/cadastro/Cadastro';
+import Atividades from '../pages/atividades/Atividades';
+import GestaoSafras from '../pages/gestaoSafras/GestaoSafras';
+import Custos from '../pages/custos/Custos';
+import Estoque from '../pages/estoque/Estoque';
+import Notificacoes from '../pages/notificacoes/Notificacoes';
+import Inicio from '../pages/inicio/inicio';
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
 
+      {/* Rotas públicas */}
       <Route path="/" element={<Homepage />} />
-      <Route path='/sobre-nos' element={<AboutUs />} />
+      <Route path="home" element={<Homepage />} />
+      <Route path="/sobre-nos" element={<AboutUs />} />
       <Route path="/login" element={<Login />} />
-  
 
+      {/* Rotas protegidas */}
       <Route element={<ProtectedRoute />}>
         <Route path="/application" element={<AppLayout />}>
-          <Route path="home" element={<h1>Home</h1>} />
           <Route path="user" element={<UserRegister />} />
           <Route path="users" element={<UserList />} />
           <Route path="reports" element={<h1>Reports</h1>} />
-          <Route path="previsao-preco" element={<PrevisaoPreco />} />
-  
+          <Route path="inicio" element={<Inicio />} />
+          <Route path="cadastro" element={<Cadastro />} />
+          <Route path="atividades" element={<Atividades />} />
+          <Route path="gestao-safras" element={<GestaoSafras />} />
+          <Route path="custos" element={<Custos />} />
+          <Route path="estoque" element={<Estoque />} />
+         <Route path="previsao-preco" element={<PrevisaoPreco />} />
+          <Route path="notificacoes" element={<Notificacoes />} />
+           
         </Route>
       </Route>
 
+      {/* Rota de erro */}
       <Route path="*" element={<h1>Página Não Encontrada (404)</h1>} />
-
     </Routes>
   );
 };
